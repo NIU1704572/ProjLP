60460eedc8a9099023a93a7152b4adc899109a1f
NIU1708093 <148786873+NIU1708093@users.noreply.github.com>
2024-11-17 12:31:36 +0100

    Update CamiBase.h

--- LP-OSM-2223-DLL/CamiBase.h
+++ LP-OSM-2223-DLL/CamiBase.h
@@ -1,11 +1,9 @@
 #pragma once
-#include "pch.h"
 
 #include "Common.h"
 #include <vector>
 
 class CamiBase {
-	public:
-		virtual std::vector<Coordinate> getCamiCoords() = 0;
+public:
+	virtual std::vector<Coordinate> getCamiCoords() = 0;
 };
-
