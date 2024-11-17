cfdb897f5b39e4b24e93dd4fa60a98e7209bfdbf
NIU1708093 <148786873+NIU1708093@users.noreply.github.com>
2024-11-17 12:36:19 +0100

    Update PuntDeInteresRestaurantSolucio.h

--- LP-OSM-2223-DLL/PuntDeInteresRestaurantSolucio.h
+++ LP-OSM-2223-DLL/PuntDeInteresRestaurantSolucio.h
@@ -5,23 +5,23 @@
 class  PuntDeInteresRestaurantSolucio :public PuntDeInteresBase {
 public:
 
-	PuntDeInteresRestaurantSolucio(const std::string& nom, const Coordinate& c, const std::string& cuisine, const bool& wheel):
-		m_cuisine(cuisine), m_wheels(wheel), PuntDeInteresBase(c, nom) {}
+	PuntDeInteresRestaurantSolucio(Coordinate c, std::string nom, std::string cuisine, bool wheel):
+		 PuntDeInteresBase(c, nom), m_cuisine(cuisine), m_wheels(wheel) {}
 
 	std::string getName() { return (PuntDeInteresBase::getName()); }
 	unsigned int getColor() { 
-			if (m_cuisine == "pizza" && m_wheels)
-				return 0x03FCBA;
-
 			if (m_cuisine == "chinese")
 				return 0xA6D9F7;
-
 			if (m_wheels)
-				return 0x251351;
-			else
-				return PuntDeInteresBase::getColor();
-		
+			{
+			    if(m_cuisine == "pizza")
+			        return 0x03FCBA;
+			    
+			    return 0x251351;
+			}
+			return PuntDeInteresBase::getColor();
 	}
+	PuntDeInteresRestaurantSolucio* clone() { return new PuntDeInteresRestaurantSolucio(*this); }
 
 private:
 	std::string m_cuisine;
@@ -48,7 +48,7 @@ private:
 		for (int i = 0; i < el.fills.size(); i++) {
 			// Iterem i avaluem tots els fills que son tags
 			if (el.fills[i].first == "tag") {
-				// Emmagatzemem el valor d’aquest tag
+				// Emmagatzemem el valor dâ€™aquest tag
 				valorTag = Util::kvDeTag(el.fills[i].second);
 				// Comprovem que es el tag que busquem
 				if (valorTag.first == "name")
@@ -57,7 +57,7 @@ private:
 
 				if (valorTag.first == "wheelchair")
 				{
-					m_wheels = (valorTag.second == "yes") ? true : false; //comprovació cadira de rodes
+					m_wheels = (valorTag.second == "yes") ? true : false; //comprovaciÃ³ cadira de rodes
 				}
 
 				if (valorTag.first == "cuisine")
