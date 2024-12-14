6d75dd1a4f060d1778d883f04959bb8d30861522
NIU1704572 <148856317+NIU1704572@users.noreply.github.com>
2024-12-14 13:44:56 +0100

    Update BallTree.cpp

--- BallTree.cpp
+++ BallTree.cpp
@@ -8,11 +8,12 @@ void BallTree::construirArbre(const std::vector<Coordinate>& coordenades) {
     // TODO: Utilitza aquest metode per construir el teu BallTree
     // TODO: amb les coordenades que se us passen per parametre
     if (!m_root)
-        m_root = this;
+        m_root = this; //el root apunta a l'arrel de l'arbre, no al pare
+
+    m_coordenades = coordenades;
 
     if (coordenades.size() > 1)
     {
-        m_coordenades = coordenades;
         vector<Coordinate> punts = coordenades; //AIXO ES PERQUE EL CALCULA PUNT CENTRAL NO ES CONST
 
         m_pivot = Util::calcularPuntCentral(punts);
@@ -24,17 +25,26 @@ void BallTree::construirArbre(const std::vector<Coordinate>& coordenades) {
         vector<Coordinate>::const_iterator itB;
         for (auto it = coordenades.begin(); it != coordenades.end(); it++)
         {
-            if (Util::DistanciaHaversine((*it), m_pivot) > dA)
+            dB = Util::DistanciaHaversine(*it, m_pivot);
+            if (dB > dA)
             {
                 itA = it;
-                m_radi = dA;
+                m_radi = dB;
+                dA = dB;
             }
 
         }
+
+        dA = 0;
         for (auto it = coordenades.begin(); it != coordenades.end(); it++)
         {
-            if (Util::DistanciaHaversine(*it, *itA) > dA)
+            dB = Util::DistanciaHaversine(*it, *itA);
+            if (dB > dA)
+            {
                 itB = it;
+                dA = dB;
+            }
+            
 
         }
 
@@ -44,8 +54,8 @@ void BallTree::construirArbre(const std::vector<Coordinate>& coordenades) {
 
         for (auto it = coordenades.begin(); it != coordenades.end(); it++)
         {
-            dA = Util::DistanciaHaversine(*it, *itA);
-            dB = Util::DistanciaHaversine(*it, *itB);
+            dA = Util::DistanciaHaversine(*itA, *it);
+            dB = Util::DistanciaHaversine(*itB, *it);
 
             if (dA < dB)
                 A.push_back(*it);
@@ -54,11 +64,11 @@ void BallTree::construirArbre(const std::vector<Coordinate>& coordenades) {
 
         }
 
-        m_left = new BallTree; 
-        m_left->m_root = this; m_left->construirArbre(A);
+        m_left = new BallTree;
+        m_left->m_root = m_root; m_left->construirArbre(A);
 
-        m_right = new BallTree; 
-        m_right->m_root = this; m_right->construirArbre(B);
+        m_right = new BallTree;
+        m_right->m_root = m_root; m_right->construirArbre(B);
     }
     else
     {
@@ -88,10 +98,10 @@ void BallTree::preOrdre(std::vector<std::list<Coordinate>>& out) {
     out.push_back(res);             //root
 
     if (m_left)
-        m_left->inOrdre(out);       //left 
+        m_left->preOrdre(out);       //left 
 
     if (m_right)
-        m_right->inOrdre(out);      //right
+        m_right->preOrdre(out);      //right
     // TODO: TASCA 2
     // 
     // TODO: TASCA 2
@@ -101,31 +111,28 @@ void BallTree::postOrdre(std::vector<std::list<Coordinate>>& out) {
     list<Coordinate> res(m_coordenades.begin(), m_coordenades.end());
 
     if (m_left)
-        m_left->inOrdre(out);       //left
+        m_left->postOrdre(out);       //left
 
     if (m_right)
-        m_right->inOrdre(out);      //right
+        m_right->postOrdre(out);      //right
 
     out.push_back(res);             //root
     // TODO: TASCA 2
 }
 
-Coordinate BallTree::nodeMesProper(Coordinate targetQuery, Coordinate& Q, BallTree* ball) {
-    // TODO: TASCA 3
-    Q.lat = 0.0;
-    Q.lon = 0.0;
-    
+Coordinate BallTree::nodeMesProperRec(Coordinate targetQuery, Coordinate& Q, BallTree* ball) {
+
     double D1 = Util::DistanciaHaversine(targetQuery, ball->m_pivot);
     double D2 = Util::DistanciaHaversine(ball->m_pivot, Q);
     if (D1 - ball->m_radi >= D2)
         return Q;
 
     if (ball->m_coordenades.size() == 1 && Util::DistanciaHaversine(targetQuery, Q) > Util::DistanciaHaversine(targetQuery, m_coordenades[0])) //no tinc 
-///////////////////                                                                                                              //clar si la segona condicio sobra
+        ///////////////////                                                                                                              //clar si la segona condicio sobra
     {
         Q = ball->m_coordenades[0]; // CREO QUE ESTO
     }
-    else
+    else if(ball->m_coordenades.size() > 1) //s'ha de mirar que no sigui fulla
     {
         double Da = Util::DistanciaHaversine(targetQuery, ball->m_left->m_pivot);
         double Db = Util::DistanciaHaversine(targetQuery, ball->m_right->m_pivot);
@@ -141,7 +148,13 @@ Coordinate BallTree::nodeMesProper(Coordinate targetQuery, Coordinate& Q, BallTr
         }
 
     }
+    return Q;
 }
 
+Coordinate BallTree::nodeMesProper(Coordinate targetQuery, Coordinate& Q, BallTree* ball) {
+    // TODO: TASCA 3
+    Q.lat = 0.0;
+    Q.lon = 0.0;
 
-
+    return nodeMesProperRec(targetQuery, Q, ball);
+}
