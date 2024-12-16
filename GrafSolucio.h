763d559014fd124d7aa6218a3c89d9b99b4e990b
NIU1704572 <148856317+NIU1704572@users.noreply.github.com>
2024-12-16 10:11:31 +0100

    Update GrafSolucio.h

--- GrafSolucio.h
+++ GrafSolucio.h
@@ -1,16 +1,30 @@
 #pragma once
 #include "pch.h"
 #include "Util.h"
-#include <list>
+#include<stack>
 #include <vector>
+#include <limits>
+const double DISTMAX = 999999999999999999.9;
+
 using namespace std;
 
 class Graf {
-	
+
 public:
-	void afegeixNode(const Coordinate c) { m_nodes.push_back(c); m_arestes.push_back(vector<Coordinate>()); }//cambiar coordinat per id
-	void afegeixAresta(const Coordinate c1, const Coordinate c2);
+	void afegeixNode(const string&, const Coordinate& c);//cambiar coordinate per id
+	void afegeixAresta(const string& n1, const string& n2);
+	void relacionaCami(const vector<string>& v);
+	void getCoords(vector<Coordinate>& v) { for (auto it = m_nodes.begin(); it != m_nodes.end(); it++) { v.push_back(it->second); } }
+
+	void camiCurt(const Coordinate& Q1, const Coordinate& Q2, stack<pair<string, Coordinate>>& cami);
+	void actualitza_nNodes() { m_numNodes = m_nodes.size(); }
 private:
-	vector<Coordinate> m_nodes;
-	vector<vector<Coordinate>> m_arestes;//canviar coordinate per id
-};
+	vector<pair<string, Coordinate>> m_nodes;//ID, Coord aquí
+	vector<vector<double>> m_arestes;//relacions
+	int m_numNodes;
+
+
+	void camiMesCurt(const string& node1, const string& node2, stack<pair<string, Coordinate>>& cami);
+	double minDistance(vector<double>& dist, vector<bool>& visitat);
+	void dijkstraModif(double node, double node2, vector<double>& dist, vector<double>& anterior);
+};/////////////////////////////MIRAR M_ARESTES EL TIPUS POT ESTAR MALAMENT
